cc8d65738d2999cc7cf80d51ffe27598b374591a
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2018-05-05 13:25:13 +0200

    //Comments added ;)

--- HID.js
+++ HID.js
@@ -4,13 +4,11 @@ var scroll = 0
 function keyPressed() {
   if(e>0){return}
   if(keyCode == LEFT_ARROW) {
-    page--
-    trans("LEFT", true, 0)
+
   } else if(keyCode == RIGHT_ARROW) {
-    page++
-    trans("RIGHT", false, 0)
+
   } else if(keyCode == 32) {  //Space
-    info = !info
+
   } else if(keyCode == 9) {   //tab
 
   } else if(keyCode == 112) { //F1
@@ -18,8 +16,7 @@ function keyPressed() {
   } else if(keyCode == 123) { //F12
 
   } else if(keyCode == 36) {  //Home
-    page=1
-    trans("LEFT", true, 0)
+    
   } else {
     return false
   }
