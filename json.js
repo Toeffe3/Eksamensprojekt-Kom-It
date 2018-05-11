751b13462b97bf0f77faf00da33e7ff0d1ebbc26
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2018-05-11 20:37:15 +0200

    Added text from json file

--- json.js
+++ json.js
@@ -1,14 +1,23 @@
 var json = [
   {
-    "Header": "Overskriften for side 1",
-    "Content": [
-      "Tekst",
-      0,
-      "Mere tekst"
-    ]
+    "from": 0,
+    "to": 100,
+    "Header": "Atomet",
+    "Content": "Atomer er lavet af protoner, elektroner og neutroner. Protoner og neutroner består af kvarks som er de bliver kaldt de ‘fundamentale partikler’."
   }, {
-
+    "from": 100,
+    "to": 200,
+    "Header": "Kvarks og leptons",
+    "Content": "Man deler de fundementale partikler ind i flere grupper. Heraf kvarks og leptons. Protoner og neutroner er bygget af kvarks, hvor imod elektronen er i sig selv en fundamental partikel; som høre ind under gruppen leptons."
   }, {
-
+    "from": 200,
+    "to": 300,
+    "Header": "Typer af kvarks",
+    "Content": "Kvarks kan være i forskellige stadier, kaldet spids, som henholdsvis hedder: Up(↑), Down(↓), Top(↥), Bottom(↧), Charm(—) og Stange(〜)."
+  }, {
+    "from": 300,
+    "to": 400,
+    "Header": "Overskriften for side 4",
+    "Content": "Tekst"
   }
 ]
