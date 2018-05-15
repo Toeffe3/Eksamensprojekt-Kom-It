90cafcda04f4649988e9b3e5a39ad3500716a05f
Lime1506 <38418425+Lime1506@users.noreply.github.com>
2018-05-15 09:35:48 +0200

    Changed text

--- json.js
+++ json.js
@@ -15,22 +15,22 @@ var json = [
     "from": 1,
     "to": 100,
     "Header": "Kort - Antimatter?",
-    "Content": "Antimatter, på dansk antistof, er en proton, elektron sågar et helt atom med omvendt ladning. Det ser ikke anderledes ud, bevæger sig ikke anderledes og hele universet kunne være lavet af antistof - intet ville være anderledes.\n\n"
+    "Content": "Antimatter (på dansk antistof) er en proton, elektron sågar et helt atom med omvendt ladning. Det ser ikke anderledes ud, bevæger sig ikke anderledes og hele universet kunne være lavet af antistof uden du ville kunne se en forskel.\n\n"Matter er således det modsatte af antimatter; protoner, elektroner og neutroner - alt det vi kan se.
   },{
     "from": 100,
     "to": 200,
     "Header": "Atomet",
-    "Content": "Atomer er lavet af protoner, elektroner og neutroner - de mindste partikler og byggestenen til alt, NEJ! Elektronen er en minste partikel og hører unden en gruppe kaldet 'Leptons', protoner og neutroner derimod består af såkaldte 'kvarks'."
+    "Content": "Atomer er lavet af protoner, elektroner og neutroner. Protoner er den positive ladet nuklid i atomet protenen står for massen af atomet.\nElektroner er elemtar partiklen der holder atomet stabilt og sørger for at atomer kan reagere med hinanden.\nNeutroner holder større atomets protoner sammen i deres kerner for at de positive protoner ikke frastøder hinanden.\n"
   }, {
     "from": 200,
     "to": 300,
-    "Header": "Kvarks og leptons",
+    "Header": "elementar partikler",
     "Content": "Man deler de fundementale partikler ind i flere grupper. Heraf kvarks og leptons. Protoner og neutroner er bygget af kvarks, hvor imod elektronen er i sig selv en fundamental partikel; som høre ind under gruppen leptons."
   }, {
     "from": 300,
     "to":400,
-    "Header": "Typer af kvarks",
-    "Content": "Kvarks kan være i forskellige stadier, kaldet spids, som henholdsvis hedder: Up(↑), Down(↓), Top(↥), Bottom(↧), Charm(—) og Stange(〜)."
+    "Header": "Spin og Antimatter",
+    "Content": "Kvarks kan være i frorskellige stadier (kaldet spids) som henholdsvis hedder: Up(↑), Down(↓), Top(↥), Bottom(↧), Charm(—) og Stange(〜)."
   }, {
     "from": 400,
     "to": 500,
