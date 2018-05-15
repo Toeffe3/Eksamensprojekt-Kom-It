3d08ac995409e328f7f0a01824074d4c18173c57
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2018-05-15 11:22:01 +0200

    Moved controles to HID.js More animations

--- json.js
+++ json.js
@@ -4,19 +4,18 @@ var json = [
     "to": 0,
     "Header": "",
     "Content": "Tryk på 'Home'-tasten"
-  },
-  {
+  }, {
     "from": 0,
     "to": 1,
     "Header": "Velkommen!",
     "Content": "Dette er en interaktiv hjemmeside, der vil lære dig hvad Antimatter/Antistof er. \n\nBrug:\nHold museknapperne nede for at bevæge dig frem eller tilbage i præsentationen. Du kan også bruge knapperne i bunden; 'Start', 'Pause', 'Forrige' og 'Næste'.",
     "Extra": "Dette er et produkt (skriftlig formidling undtaget) lavet af Victor Jacobsen til Emil Mattisen, Anders Dænker og Victor Jacobsen's Kommikation/It-eksamen."
-  },{
+  }, {
     "from": 1,
     "to": 100,
     "Header": "Kort - Antimatter?",
-    "Content": "Antimatter (på dansk antistof) er en proton, elektron sågar et helt atom med omvendt ladning. Det ser ikke anderledes ud, bevæger sig ikke anderledes og hele universet kunne være lavet af antistof uden du ville kunne se en forskel.\n\n"Matter er således det modsatte af antimatter; protoner, elektroner og neutroner - alt det vi kan se.
-  },{
+    "Content": "Antimatter (på dansk antistof) er en proton, elektron sågar et helt atom med omvendt ladning. Det ser ikke anderledes ud, bevæger sig ikke anderledes og hele universet kunne være lavet af antistof uden du ville kunne se en forskel.\n\nMatter er således det modsatte af antimatter; protoner, elektroner og neutroner - alt det vi kan se."
+  }, {
     "from": 100,
     "to": 200,
     "Header": "Atomet",
--- pages.js
+++ pages.js
@@ -1,6 +1,4 @@
 function startpage() {
-
-
   if(dir) {
     bColor++
     fColor--
@@ -52,9 +50,9 @@ function startpage() {
 
 }
 
-function atom() {        //x is used to modify behavior for som part of the function
-  if (frame > 100 && frame < 300) {               //If x is not defined or false do:
-    atomSize=(frame-100)*2      //Increase atom
+function atom() {
+  if (frame > 50 && frame < 75) { //If x is not defined or false do:
+    atomSize=(frame-50)*12        //Increase atom
   }
 
   var eX = cos(frameCount*0.04)*(150+atomSize/1.55) //Create or re-define the electron's x position (Using cos to make it orbit)
@@ -68,7 +66,7 @@ function atom() {        //x is used to modify behavior for som part of the func
   ellipse(area.C.w,area.C.h,50+atomSize)
   ellipse(area.C.w+eX,area.C.h+eY,20+atomSize/10)
 
-  if (!betw(200, 400)) {                //If x is true do:
+  if (!betw(75, 150)) { //If x is true do:
     fill(dark)          //Set fill-color
     textSize(18)        //Set font/text size
     noStroke()
@@ -80,21 +78,48 @@ function atom() {        //x is used to modify behavior for som part of the func
 
 function quark() {
   fill(bright)
-  ellipse(area.C.w,area.C.h+90,100)
-  ellipse(area.C.w-75,area.C.h-65,100)
-  ellipse(area.C.w+75,area.C.h-65,100)
+  ellipse(area.C.w,area.C.h+90,80)
+  ellipse(area.C.w-75,area.C.h-65,80)
+  ellipse(area.C.w+75,area.C.h-65,80)
 
   textAlign(CENTER)
   fill(dark)
   noStroke()
-  text("DOWN",area.C.w,area.C.h+90+10)
-  text("UP",area.C.w-75,area.C.h-65+5)
-  text("UP",area.C.w+75,area.C.h-65+5)
+  textSize(28)
+  textStyle(BOLD)
+  text("↓",area.C.w,area.C.h+90+1)
+  text("↑",area.C.w-75,area.C.h-65)
+  text("↑",area.C.w+75,area.C.h-65)
+  textSize(10)
+  text("down",area.C.w,area.C.h+90+16)
+  text("up",area.C.w-75,area.C.h-65+14)
+  text("up",area.C.w+75,area.C.h-65+14)
   stroke(dark)
+  textStyle(NORMAL)
+
+  if (frame >= 15) {
+
+  }
+}
+
+function forcet() {
+
+  fill(bright)
+  rect(0,0,area.C.w,area.h)
+  fill(dark)
+  ellipse(area.C.w-100, -20+grav, 75)
+
+  rect(area.C.w, 0, area.w,area.h)
+  fill(bright)
+  ellipse(area.C.w+100, -20+grav, 75)
+  grav = sqrt(sq(grav)) * 1.5
+  if (grav > area.h*10) {
+    grav = 1
+  }
 }
 
 function collide() {
-  var dis = 200-(frame-500)*(frame-500)
+  var dis = 200-(frame-350)*(frame-350)
   fill(bright)
   rect(0,0,area.C.w,area.h)
   fill(dark)
@@ -105,16 +130,20 @@ function collide() {
   ellipse(area.C.w+dis, area.C.h, 75)
 
   if(dis <= 75) {
-    ellipse(area.C.w, area.C.h, (frame-500)*(frame-500)*2)
+    ellipse(area.C.w, area.C.h, (frame-350)*(frame-350)*2)
 
-    if (frame > 518) {
+    if (frame > 368) {
       fill(dark)
-      ellipse(area.C.w, area.C.h, ((frame-500)*(frame-500)*2)-900)
+      ellipse(area.C.w, area.C.h, ((frame-350)*(frame-350)*2)-900)
 
-      if (frame > 523) {
+      if (frame > 373) {
         fill(bright)
-        ellipse(area.C.w, area.C.h, ((frame-500)*(frame-500)*2)-1100)
+        ellipse(area.C.w, area.C.h, ((frame-350)*(frame-350)*2)-1100)
       }
     }
   }
+}
+
+function eequalmcsquared() {
+
 }
