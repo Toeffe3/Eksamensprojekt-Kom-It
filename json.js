323b003d27655185fc9b0d4d10fff404ed383bb6
Lime1506 <38418425+Lime1506@users.noreply.github.com>
2018-05-16 08:34:30 +0200

    addede til de siste slides

--- json.js
+++ json.js
@@ -13,28 +13,48 @@ var json = [
     "Extra": "Dette er et produkt (skriftlig formidling undtaget) lavet af Victor Jacobsen til Emil Mattisen, Anders Dænker og Victor Jacobsen's Kommikation/It-eksamen."
   },{
     "from": 1,
-    "to": 100,
+    "to": 50,
     "Header": "Kort - Antimatter?",
     "Content": "Antimatter (på dansk antistof) er en proton, elektron sågar et helt atom med omvendt ladning. Det ser ikke anderledes ud, bevæger sig ikke anderledes og hele universet kunne være lavet af antistof uden du ville kunne se en forskel.\n\n"Matter er således det modsatte af antimatter; protoner, elektroner og neutroner - alt det vi kan se.
   },{
-    "from": 100,
-    "to": 200,
+    "from": 50,
+    "to": 75,
     "Header": "Atomet",
     "Content": "Atomer er lavet af protoner, elektroner og neutroner. Protoner er den positive ladet nuklid i atomet protenen står for massen af atomet.\nElektroner er elemtar partiklen der holder atomet stabilt og sørger for at atomer kan reagere med hinanden.\nNeutroner holder større atomets protoner sammen i deres kerner for at de positive protoner ikke frastøder hinanden.\n"
   }, {
-    "from": 200,
-    "to": 300,
+    "from": 75,
+    "to": 150,
     "Header": "elementar partikler",
     "Content": "Man deler de fundementale partikler ind i flere grupper. Heraf kvarks og leptons. Protoner og neutroner er bygget af kvarks, hvor imod elektronen er i sig selv en fundamental partikel; som høre ind under gruppen leptons."
   }, {
+    "from": 150,
+    "to":200,
+    "Header": "Antimatter Spind",
+    "Content": "Protoner og neutroner er bygget af kvarks, hvor elektronen i sig selv en fundamental partikel; som hører ind under gruppen leptons. Kvarks kan være i forskellige stadier (kaldet spids) som henholdsvis hedder: Up(↑), Down(↓), Top(↥), Bottom(↧), Charm(—) og Stange(〜). Disse stadier er opdelt i par af to modsætninger som; (Up(↑), Down(↓)), (Top(↥), Bottom(↧)) og (Charm(—), Strange(〜)). Kvarks laver en trippel-forbindelse. som bestemmer om det er en proton eller en neutron. En neutron består af en Up(↑) og 2 Down(↓). Neutronen er neutral da, Up(↑) har en ladning på +⅔, mens Down(↓) har en ladning på -⅓. En proton bestårb så af 2 Up(↑) og en Down(↓)."
+  }, {
+    "from": 200,
+    "to": 300,
+    "Header": "Egenskaber 1",
+    "Content": "Tekst"
+  }, {
     "from": 300,
-    "to":400,
-    "Header": "Spin og Antimatter",
-    "Content": "Kvarks kan være i frorskellige stadier (kaldet spids) som henholdsvis hedder: Up(↑), Down(↓), Top(↥), Bottom(↧), Charm(—) og Stange(〜)."
+    "to": 350,
+    "Header": "Egenskaber 2",
+    "Content": "Tekst"
+  }, {
+    "from": 350,
+    "to": 400,
+    "Header": "E=mc2 1",
+    "Content": "Tekst"
   }, {
     "from": 400,
-    "to": 500,
-    "Header": "Overskriften for side 4",
+    "to": 450,
+    "Header": "E=mc2 2",
+    "Content": "Tekst"
+  }, {
+    "from": 450,
+    "to": 550,
+    "Header": "Opgaver/?",
     "Content": "Tekst"
   }
 ]
